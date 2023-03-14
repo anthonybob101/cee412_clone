f3cbcb9bca937c41dec748e84d0004681e6b8dcd
anthonybob101 <34084168+anthonybob101@users.noreply.github.com>
2023-03-13 22:11:12 -0700

    added in pages to sidebar

---
+++ homepage.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+"# vehicle accidents"
+"## are young people worse at driving?"
+"probably"
+
+'''
+## a StReAmLiT app
+by Henry, Tung and Tony
+'''
+# Contents of ~/my_app/main_page.py
+st.markdown("# Main page 🎈")
+st.sidebar.markdown("# Main page 🎈")
+
+# Contents of ~/my_app/pages/page_2.py
+import streamlit as st
+
+st.markdown("# Page 2 ❄️")
+st.sidebar.markdown("# Page 2 ❄️")
+
+# Contents of ~/my_app/pages/page_3.py
+import streamlit as st
+
+st.markdown("# Page 3 🎉")
+st.sidebar.markdown("# Page 3 🎉")
